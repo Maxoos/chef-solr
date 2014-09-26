6d3c7a1d56a14260de602f98ed9bec974fe36911
Maxoos <max.penson@gmail.com>
2014-09-26 23:04:46 +0300

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@
 
 default['solr']['version']  = '4.6.1'
 default['solr']['url']      = "https://archive.apache.org/dist/lucene/solr/#{node['solr']['version']}/solr-#{node['solr']['version']}.tgz"
-default['solr']['data_dir'] = '/etc/solr'
+default['solr']['data_dir'] = '/solr'
 default['solr']['dir']      = '/opt/solr'
 default['solr']['port']     = '8984'
 default['solr']['pid_file'] = '/var/run/solr.pid'
